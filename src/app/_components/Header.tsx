937d94d5a46bd15e28c2bf52c8ec2d0a933384e0
Anna Grace Raju <annagraceraju123@gmail.com>
2025-02-20 12:24:13 +0530

    Update header display

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,13 +1,12 @@
-
-function Header(){
-    return(
-      <div className="flex justify-between h-16 items-center top-0 left-0 bg-blue-500">
-        <h1 className="ml-8 text-3xl">LOGO</h1>
-        <ul className="flex gap-5 text-2xl">
-            <li className="mr-8 ">Log In</li>
-            <li className="mr-8 ">Sign In</li>
-        </ul>
-      </div>
-    );
+function Header() {
+  return (
+    <div className="fixed top-0 left-0 w-full flex justify-between h-16 items-center bg-blue-500 text-white shadow-md z-50">
+      <h1 className="ml-8 text-3xl">LOGO</h1>
+      <ul className="flex gap-5 text-2xl">
+        <li className="mr-8 cursor-pointer hover:text-gray-300">Log In</li>
+        <li className="mr-8 cursor-pointer hover:text-gray-300">Sign In</li>
+      </ul>
+    </div>
+  );
 }
 export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-start items-center h-screen bg-gray-900 text-white">
-      <h1 className="text-red-700 text-5xl mt-16 mb-8">App Page</h1>
+    <div className="flex flex-col justify-start items-center h-screen bg-gray-900 text-white mt-16">
+      <h1 className="text-red-700 text-5xl mt-16 mb-8">Welcome Page</h1>
       <p className="text-center border border-yellow-500 p-8 w-1/2">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, dicta beatae at libero nulla illum repellendus qui rem quidem ipsam ut ipsa, nam tenetur. Architecto repellendus minus ipsum autem illum.
       </p>
