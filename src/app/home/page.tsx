7efe26e9ece240340591e912cfc3b70ff8b1323d
Anna Grace Raju <annagraceraju123@gmail.com>
2025-02-20 12:54:10 +0530

    updates heading

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Page() {
     return (
         <div className="flex flex-col justify-start items-center h-screen bg-gray-900 text-white">
-            <h1 className="text-yellow-500 text-5xl mt-32 mb-8 font-bold">Welcome Home</h1>
+            <h1 className="text-yellow-500 text-5xl mt-32 mb-8 font-bold">Home</h1>
             <p className="text-center border border-yellow-500 p-8 w-1/2">
                 Discover a world of opportunities with our platform. Whether you're here to learn, explore, or connect, we provide the tools and resources to help you thrive. Join us today and take the next step toward success!
             </p>
